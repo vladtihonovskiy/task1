f59626bee37d27143f078866077fdac500fc4200
vlad tihonovskiy <balyko.peaceit@gmail.com>
2018-10-21 18:30:33 +0300

    done

--- src/index.js
+++ src/index.js
@@ -4,7 +4,52 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const world = 'world';
+const string = <h1>Hello, {world}</h1>;
+
+
+//ReactDOM.render(string, document.getElementById('root'));
+
+
+function formatName(firstName, lastName) {
+	return firstName + ' ' + lastName;
+}
+
+
+// const element = (
+// 	<h1>
+// 		Hello, { formatName("Vlad", "Tihonoskiy") }!
+// 	</h1>
+// );
+
+
+//ReactDOM.render(element, document.getElementById('root'));
+
+
+
+// const element = (
+// 	<div>
+// 		<h1>Hello!</h1>
+// 		<h2>Good to see you here.</h2>
+// 	</div>
+// );
+//
+//
+// ReactDOM.render(element, document.getElementById('root'));
+
+
+//
+// function Welcome(props) {
+// 	return <h1>Hello, {props.name}</h1>;
+// }
+//
+// ReactDOM.render(Welcome({name:"vlad"}), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
